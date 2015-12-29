d1ace72be04b63a122d0de8f5366c084857eeed9
Jonathan Badger <jhbadger@gmail.com>
2015-12-29 16:33:59 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,281 @@
+(require 'cl)
+(load "package")
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(setq inferior-julia-program-name "julia")
+(package-initialize)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+(defvar packages '(ac-slime
+			  auto-complete
+			  autopair
+			  clojure-mode
+			  coffee-mode
+			  csharp-mode
+			  deft
+			  erlang
+			  feature-mode
+			  flycheck
+			  gist
+			  go-autocomplete
+			  go-eldoc
+			  go-mode
+			  graphviz-dot-mode
+			  haml-mode
+			  haskell-mode
+			  htmlize
+			  idris-mode
+			  magit
+			  markdown-mode
+			  marmalade
+			  nodejs-repl
+			  o-blog
+			  org
+			  paredit
+			  php-mode
+			  puppet-mode
+			  restclient
+			  rvm
+			  scala-mode
+			  smex
+			  sml-mode
+			  solarized-theme
+			  web-mode
+			  writegood-mode
+			  yaml-mode
+			  julia-mode
+			  picolisp-mode
+			  warm-night-theme
+			  )
+    "Default packages")
+
+(defun packages-installed-p ()
+  (loop for pkg in packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
+
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
+
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+(setq make-backup-files nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c C-k") 'compile)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+(show-paren-mode t)
+
+(setq org-log-done t
+      org-todo-keywords '((sequence "TODO" "INPROGRESS" "DONE"))
+      org-todo-keyword-faces '(("INPROGRESS" . (:foreground "blue" :weight bold))))
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (flyspell-mode)))
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (writegood-mode)))
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-agenda-show-log t
+      org-agenda-todo-ignore-scheduled t
+      org-agenda-todo-ignore-deadlines t)
+(setq org-agenda-files (list "~/Dropbox/org/personal.org"
+			     "~/Dropbox/org/groupon.org"))
+
+(require 'org)
+(require 'org-install)
+(require 'org-habit)
+(add-to-list 'org-modules "org-habit")
+(setq org-habit-preceding-days 7
+      org-habit-following-days 1
+      org-habit-graph-column 80
+      org-habit-show-habits-only-for-today t
+      org-habit-show-all-today t)
+
+(setq org-src-fontify-natively t)
+
+(add-hook 'org-babel-after-execute-hook (lambda ()
+					  (condition-case nil
+					      (org-display-inline-images)
+					    (error nil)))
+	            'append)
+
+(setq deft-directory "~/Dropbox/deft")
+(setq deft-use-filename-as-title t)
+(setq deft-extension "org")
+(setq deft-text-mode 'org-mode)
+
+(setq column-number-mode t)
+
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+(require 'autopair)
+
+(setq lisp-modes '(lisp-mode
+		   emacs-lisp-mode
+		   common-lisp-mode
+		   scheme-mode
+		   clojure-mode))
+
+(defvar lisp-power-map (make-keymap))
+(define-minor-mode lisp-power-mode "Fix keybindings; add power."
+  :lighter " (power)"
+  :keymap lisp-power-map
+  (paredit-mode t))
+(define-key lisp-power-map [delete] 'paredit-forward-delete)
+(define-key lisp-power-map [backspace] 'paredit-backward-delete)
+
+(defun engage-lisp-power ()
+  (lisp-power-mode t))
+
+(dolist (mode lisp-modes)
+  (add-hook (intern (format "%s-hook" mode))
+	    #'engage-lisp-power))
+
+(setq inferior-lisp-program "clisp")
+(setq scheme-program-name "racket")
+
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+(setq-default show-trailing-whitespace t)
+
+(setq flyspell-issue-welcome-flag nil)
+(if (eq system-type 'darwin)
+    (setq-default ispell-program-name "/usr/local/bin/aspell")
+  (setq-default ispell-program-name "/usr/bin/aspell"))
+(setq-default ispell-list-command "list")
+
+(add-to-list 'auto-mode-alist '("\\.hbs$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (autopair-mode)))
+
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (make-local-variable 'tab-width)
+  (set 'tab-width 2))
+
+(add-hook 'coffee-mode-hook 'coffee-custom)
+
+(defun js-custom ()
+  "js-mode-hook"
+  (setq js-indent-level 2))
+
+(add-hook 'js-mode-hook 'js-custom)
+
+(require 'go-autocomplete)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (go-eldoc-setup)
+            (add-hook 'before-save-hook 'gofmt-before-save)))
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(global-set-key [f7] 'paredit-mode)
+(global-set-key "\C-cd" 'dash-at-point)
+(global-set-key "\C-ce" 'dash-at-point-with-docset)
+(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+(add-hook 'clojurescript-mode-hook #'inf-clojure-minor-mode)
+(defun xlispstat ()
+  (interactive)
+  (inferior-lisp "xlispstat"))
+(defun planck ()
+  (interactive)
+  (inf-clojure "planck"))
+(setq calendar-latitude 39.0836)
+(setq calendar-longitude -77.1483)
+(setq calendar-location-name "Rockville, MD")
+(add-to-list 'auto-mode-alist '("\\.l$" . picolisp-mode))
+
+(org-babel-do-load-languages
+   'org-babel-load-languages
+   '((R . t)
+     (julia .t)
+     (ruby . t)
+     (python . t)
+     (clojure . t)
+     (lisp . t)
+     (picolisp . t)
+     ))
+(setq org-confirm-babel-evaluate nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (warm-night)))
+ '(custom-safe-themes
+   (quote
+    ("ac2b1fed9c0f0190045359327e963ddad250e131fbf332e80d371b2e1dbc1dc4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
