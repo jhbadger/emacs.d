582b3335cd2070f35cb9a7a08754dcdee09e3436
Jonathan Badger <jhbadger@gmail.com>
2015-12-31 10:16:54 -0500

    paths, xlispstat

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 (require 'cl)
 (load "package")
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (setq inferior-julia-program-name "julia")
 (package-initialize)
@@ -49,6 +58,7 @@
 			  julia-mode
 			  picolisp-mode
 			  warm-night-theme
+			  inf-clojure
 			  )
     "Default packages")
 
