adf7fd9a75453c8984d04319e365c7d31c0c1b65
Jonathan Badger <jhbadger@gmail.com>
2017-11-09 17:05:04 -0500

    more stat functions

--- init.el
+++ init.el
@@ -102,6 +102,42 @@
   "return standard deviation of list"
   (sqrt (var s)))
 
+(defun standardize (s)
+  "return standardized form of list s with mean 0 and sd 1"
+  (let ((m (mean s)) (std (sd s)))
+    (mapcar (lambda (x) (/ x std))
+	    (mapcar (lambda (x) (- x m))  s))))
+
+(defun simple-inserter (insert-fun)
+  (lambda (acc next)
+    (if (listp next)
+        (funcall insert-fun acc next)
+        (list next acc))))
+
+(defmacro -> (initial-form &rest forms)
+  "Inserts INITIAL-FORM as first argument into the first of FORMS, the result
+into the next, etc., before evaluation.  FORMS are treated as list designators."
+  (cl-reduce (simple-inserter #'insert-first)
+          forms
+          :initial-value initial-form))
+
+(defmacro ->> (initial-form &rest forms)
+  "Like ->, but the forms are inserted as last argument instead of first."
+  (cl-reduce (simple-inserter #'insert-last)
+          forms
+          :initial-value initial-form))
+
+(defun insert-first (arg surround)
+  "Inserts ARG into the list form SURROUND as its first argument, after the
+operator."
+  (cl-list* (car surround)
+         arg
+         (cdr surround)))
+
+(defun insert-last (arg surround)
+  "Inserts ARG into the list form SURROUND as its last argument."
+  (append surround (list arg)))
+
 ;; add to hook
 (add-hook 'nim-mode-hook 'my-nim-mode-config)
 
