e104dd12f1dc7ff008554dbcc434f72cc0cfe3a3
Jonathan Badger <jhbadger@gmail.com>
2019-07-14 08:38:19 -0400

    company mode

---
+++ elpa/clojure-mode-20190712.639/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20190712.639" "Major mode for Clojure code" '((emacs "25.1")) :commit "c7171e4546f5ed38d3b07cdd8d069e97496ab777" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
