d1287fbd40428f795418dd824ce9b6dd0e64066d
Jonathan Badger <jhbadger@gmail.com>
2019-07-14 08:38:44 -0400

    company mode

--- init.el
+++ init.el
@@ -10,34 +10,21 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zig-mode malyon haskell-mode elm-mode w3m nov paredit-menu auto-complete ess ob-hy hy-mode crystal-mode slime ob-nim ob-prolog ruby-compilation nim-mode cider))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    (company zig-mode malyon haskell-mode elm-mode w3m nov paredit-menu ess ob-hy hy-mode crystal-mode ob-nim ob-prolog ruby-compilation nim-mode cider))))
 
-(slime-setup '(slime-repl   ; <<----- 
-               slime-mrepl 
-               slime-asdf 
-               slime-sprof 
-               slime-compiler-notes-tree 
-               slime-hyperdoc 
-               slime-indentation 
-               slime-media 
-               slime-fancy))
-(global-smart-tab-mode 1) ;; switch on smart-tab everywhere
+(add-hook 'after-init-hook 'global-company-mode)
 
-(setq smart-tab-completion-functions-alist 
-      '((emacs-lisp-mode . lisp-complete-symbol) 
-        (text-mode . dabbrev-completion) ;; this is the "default" emacs expansion function
-        (lisp-mode . slime-complete-symbol)
-	(slime-repl-mode . slime-complete-symbol)))
+; No delay in showing suggestions.
+(setq company-idle-delay 0)
+; Show suggestions after entering one character.
+(setq company-minimum-prefix-length 1)
 
 
+(eval-after-load 'cider
+  '(progn
+     (add-hook 'clojure-mode-hook 'cider-mode)
+     (add-hook 'cider-repl-mode-hook 'paredit-mode)))
 
-(ac-config-default)
 (setq org-confirm-babel-evaluate nil)
 ;; active Babel languages
 (org-babel-do-load-languages
@@ -49,7 +36,7 @@
    (hy . t)
    (nim . t)
    ))
-(setenv "RUBYLIB" "/Users/badgerjh/scripts")
+(setenv "RUBYLIB" (concat (getenv "HOME") "/scripts"))
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (setenv "PATH" (concat (getenv "HOME") "/bin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/local/bin" exec-path))
@@ -60,7 +47,9 @@
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 
 (global-set-key [f5] 'compile)
-(global-set-key [f7] 'paredit-mode)
+(global-set-key (kbd "M-p") 'paredit-mode)
+(global-set-key (kbd "M-c") 'compile)
+
 (defun my-nim-mode-config ()
   "For use in `nim-mode-hook'."
   (local-set-key (kbd "<f5>") 'nim-compile) ; add a key
