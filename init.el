2e3e1d7617b063e5ad20f7bd3c78c8841bbd662e
Jonathan Badger <jhbadger@gmail.com>
2020-06-01 12:57:12 -0400

    consistent

--- init.el
+++ init.el
@@ -12,7 +12,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-coq proof-general sly smalltalk-mode company zig-mode malyon haskell-mode elm-mode w3m nov paredit-menu ess ob-hy hy-mode crystal-mode ob-nim ob-prolog ruby-compilation nim-mode cider))))
+    (sly smalltalk-mode company zig-mode malyon haskell-mode elm-mode w3m nov paredit-menu ess ob-hy hy-mode crystal-mode ob-nim ob-prolog ruby-compilation nim-mode cider))))
+
+(setq calendar-latitude 39.1)
+(setq calendar-longitude -77.15)
+(setq calendar-location-name "Rockville, MD")
 
 (add-hook 'after-init-hook 'global-company-mode)
 
@@ -68,60 +72,7 @@
 (add-hook 'racket-mode-hook 'my-racket-mode-config)
 (add-hook 'racket-repl-mode-hook 'my-racket-mode-config)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-;; Proof General
-;; Hybrid mode is by far the best.
-(setq proof-three-window-mode-policy 'hybrid)
-
-;;; I don't know who wants to evaluate comments
-;;; one-by-one, but I don't.
-(setq proof-script-fly-past-comments t)
-
-(with-eval-after-load 'coq
-  ;; The most common command by far. Having a 3(!)
-  ;; keys long sequence for this command is just a
-  ;; crime.
-  (define-key coq-mode-map "\M-n"
-    #'proof-assert-next-command-interactive)
-
-  ;; Proof navigation didn't work for me. So please
-  ;; stand aside for my paragraph navigation.
-  ;; https://endlessparentheses.com/meta-binds-part-2-a-peeve-with-paragraphs.html
-  (define-key coq-mode-map "\M-e" nil)
-  (define-key coq-mode-map "\M-a" nil)
-
-  ;; Small convenience for commonly written commands.
-  (define-key coq-mode-map "\C-c\C-m" "\nend\t")
-  (define-key coq-mode-map "\C-c\C-e"
-    #'endless/qed)
-  (defun endless/qed ()
-    (interactive)
-    (unless (memq (char-before) '(?\s ?\n ?\r))
-      (insert " "))
-    (insert "Qed.")
-    (proof-assert-next-command-interactive)))
-
-(defun open-after-coq-command ()
-  (when (looking-at-p " *(\\*")
-    (open-line 1)))
-
-(advice-add 'proof-assert-next-command-interactive
-            :after #'open-after-coq-command)
-
-(define-abbrev-table 'coq-mode-abbrev-table '())
-(define-abbrev coq-mode-abbrev-table "re" "reflexivity.")
-(define-abbrev coq-mode-abbrev-table "id" "induction")
-(define-abbrev coq-mode-abbrev-table "si" "simpl.")
-(advice-add 'proof-assert-next-command-interactive
-            :before #'expand-abbrev)
-(when (fboundp 'company-coq-initialize)
-  (add-hook 'coq-mode-hook #'company-coq-initialize))
 
 
   
